cc7ee3dec5e02246b2c5c22336ce6a69bed540b2
DevCoopa <121086220+DevCoopa@users.noreply.github.com>
2023-01-04 09:33:03 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("#canvas");
+const c = canvas.getContext("2d");
+
+const canvas_width = 600;
+const canvas_height = 400;
+
+canvas.width = canvas_width;
+canvas.height = canvas_height;
+
+c.fillStyle("#6EEDFF")
+c.draw(0, 0, canvas_width, canvas_height);
+c.fill();
